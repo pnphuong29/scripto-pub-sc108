e636cdecc3ef2e2071bb0d02829c0821cb87b57f
Albert Pham <pnphuong29@gmail.com>
2023-12-18 17:21:51 +0700

    Auto commit

--- libs/ap_func_1.1_dir_struct.sh
+++ libs/ap_func_1.1_dir_struct.sh
@@ -3,31 +3,31 @@ ap_func_create_dir_struct_core() {
     @logshow "Create [core] directories, files and symlinks\n"
 
     # Symlinks
-    @logshow "Create directory [${AP_SYMLINKS_DIR}]\n"
+    @logshow "Create directories [${AP_SYMLINKS_DIR}]\n"
     mkdir -p "${AP_SYMLINKS_DIR}"
 
     # Sofware
-    @logshow "Create directory [${AP_SOFT_DIR}]\n"
-    mkdir -p "${AP_SOFT_DIR}"
+    @logshow "Create directories [${AP_SOFT_DIR}/bin]\n"
+    mkdir -p "${AP_SOFT_DIR}/bin"
 
     # Configs
-    @logshow "Create directory [${AP_CONFIGS_DIR}]\n"
+    @logshow "Create directories [${AP_CONFIGS_DIR}]\n"
     mkdir -p "${AP_CONFIGS_DIR}"
 
     # Man pages
-    @logshow "Create directory [${AP_MAN_DIR}]\n"
-    mkdir -p "${AP_MAN_DIR}"
+    @logshow "Create directories [${AP_MAN_DIR}/man{1..8}]\n"
+    mkdir -p "${AP_MAN_DIR}/man"{1..8}
 
     # Autocomplete
-    @logshow "Create directory [${AP_COMPLETIONS_DIR}]\n"
+    @logshow "Create directories [${AP_COMPLETIONS_DIR}]\n"
     mkdir -p "${AP_COMPLETIONS_DIR}"
 
     # Tmp
-    @logshow "Create directory [${AP_TMP_DIR}]\n"
+    @logshow "Create directories [${AP_TMP_DIR}]\n"
     mkdir -p "${AP_TMP_DIR}"
 
     # Logs
-    @logshow "Create directory [${AP_LOGS_DIR}/general] and log files\n"
+    @logshow "Create directories [${AP_LOGS_DIR}/general] and log files\n"
     mkdir -p "${AP_LOGS_DIR}/general"
     [ ! -f "${AP_LOGS_DIR}/general/ap_general.log" ] && touch "${AP_LOGS_DIR}/general/ap_general.log"
     [ ! -f "${AP_LOGS_DIR}/general/ap_error.log" ] && touch "${AP_LOGS_DIR}/general/ap_error.log"
