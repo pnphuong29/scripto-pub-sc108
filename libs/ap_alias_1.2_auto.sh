61aa3564043b2be791e741db7385602dbec55a7e
Albert Pham <pnphuong29@gmail.com>
2023-12-26 10:30:05 +0700

    Auto commit

--- libs/ap_alias_1.2_auto.sh
+++ libs/ap_alias_1.2_auto.sh
@@ -4,7 +4,7 @@ for ap_vendor in "${AP_SCRIPTO_DIR}/vendors"/ap_vendor_*.sh; do
     ap_vendor_name="${ap_vendor_name/\.sh/}"
     ap_vendor_name="${ap_vendor_name/_/}"
 
-    # Alias - Vi commands
+    # Alias - Vi sc commands
     if [ -f "${AP_SCRIPTO_COMMON_DIR}/vendors/ap_vendor_${ap_vendor_name}.sh" ]; then
         ap_cmd="alias @visc${ap_vendor_name}='@vi -p \
             ${AP_SCRIPTO_DIR}/vendors/ap_vendor_${ap_vendor_name}.sh \
@@ -24,10 +24,24 @@ for ap_vendor in "${AP_SCRIPTO_COMMON_DIR}/vendors"/ap_vendor_*.sh; do
     ap_vendor_name="${ap_vendor_name/ap_vendor_/}"
     ap_vendor_name="${ap_vendor_name/\.sh/}"
 
-    # Alias - Vi commands
+    # Alias - Vi sc commands
     ap_cmd="alias @visc${ap_vendor_name}='@vi -p \
         ${AP_SCRIPTO_DIR}/vendors/ap_vendor_${ap_vendor_name}.sh \
         ${AP_SCRIPTO_COMMON_DIR}/vendors/ap_vendor_${ap_vendor_name}.sh \
+	'"
+    # @minfo "Execute command [${ap_cmd}]\n"
+    eval "$(printf "%s" "${ap_cmd}")"
+done
+
+for ap_dkc_file in "${AP_SCRIPTO_COMMON_DIR}/dockers"/ap_dkc_*.yml; do
+    ap_dkc_name="${ap_dkc_file##*/}"
+    ap_dkc_code="${ap_dkc_name/ap_dkc_/}"
+    ap_dkc_code="${ap_dkc_code/\.yml/}"
+    ap_dkc_code="${ap_dkc_code/_/}"
+
+    # Alias - Vi docker commands
+    ap_cmd="alias @vidk${ap_dkc_code}='@vi -p \
+        ${ap_dkc_file} \
 	'"
     # @minfo "Execute command [${ap_cmd}]\n"
     eval "$(printf "%s" "${ap_cmd}")"
