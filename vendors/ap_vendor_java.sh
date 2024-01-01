e6c8d877d41d921dab2a620351f95f9bb9ce9534
Albert Pham <pnphuong29@gmail.com>
2024-01-01 09:25:18 +0700

    Auto commit

--- vendors/ap_vendor_java.sh
+++ vendors/ap_vendor_java.sh
@@ -1,13 +1,13 @@
-export AP_JDK_DL_VERSION='21'
+export AP_JDK_SETUP_VERSION='21'
 
 alias @initjava="ap_func_init_java"
 ap_func_init_java() {
-    local ap_jdk_dl_dir="jdk-${AP_JDK_DL_VERSION}.jdk"
+    local ap_jdk_dl_dir="jdk-${AP_JDK_SETUP_VERSION}.jdk"
     local ap_vendors_java_dir="${AP_SOFT_DIR}/java/${ap_jdk_dl_dir}"
     local ap_bin_path="${ap_vendors_java_dir}/Contents/Home/bin"
 
     if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
-        ap_jdk_dl_dir="jdk-${AP_JDK_DL_VERSION}"
+        ap_jdk_dl_dir="jdk-${AP_JDK_SETUP_VERSION}"
         ap_vendors_java_dir="${AP_SOFT_DIR}/java/${ap_jdk_dl_dir}"
         ap_bin_path="${ap_vendors_java_dir}/bin"
     fi
@@ -15,38 +15,84 @@ ap_func_init_java() {
     if [ -d "${ap_bin_path}" ]; then
         @addpath "${ap_bin_path}"
     fi
+
+    if alias @initjavashare &>/dev/null; then
+        @initjavashare
+    fi
+
+    if alias @initjavacommon &>/dev/null; then
+        @initjavacommon
+    fi
+}
+
+alias @createdirstructjava="ap_func_create_dirstruct_java"
+ap_func_create_dirstruct_java() {
+    if alias @createdirstructjavashare &>/dev/null; then
+        @createdirstructjavashare
+    fi
+
+    if alias @createdirstructjavacommon &>/dev/null; then
+        @createdirstructjavacommon
+    fi
+}
+
+alias @rmdirstructjava="ap_func_rm_dirstruct_java"
+ap_func_rm_dirstruct_java() {
+    if alias @rmdirstructjavashare &>/dev/null; then
+        @rmdirstructjavashare
+    fi
+
+    if alias @rmdirstructjavacommon &>/dev/null; then
+        @rmdirstructjavacommon
+    fi
 }
 
 alias @setupjava="ap_func_setup_java"
 ap_func_setup_java() {
     # https://www.oracle.com/java/technologies/downloads/
-    @logshow "Install [jdk ${AP_JDK_DL_VERSION}]\n"
+
+    local ap_java_setup_version='21'
+    if [ -n "$1" ]; then
+        ap_java_setup_version="$1"
+    fi
+
+    @logshow "Install [JDK v${ap_java_setup_version}]\n"
 
     local ap_os="macos"
-    local ap_jdk_dl_dir="jdk-${AP_JDK_DL_VERSION}.jdk"
+    local ap_jdk_dl_dir="jdk-${AP_JDK_SETUP_VERSION}.jdk"
     local ap_vendors_java_dir="${AP_SOFT_DIR}/java/${ap_jdk_dl_dir}"
 
     if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
         ap_os="linux"
-        ap_jdk_dl_dir="jdk-${AP_JDK_DL_VERSION}.0.1"
+        ap_jdk_dl_dir="jdk-${AP_JDK_SETUP_VERSION}.0.1"
         ap_vendors_java_dir="${AP_SOFT_DIR}/java/${ap_jdk_dl_dir}"
     fi
 
     cd "${AP_TMP_DIR}"
-    rm -rf "jdk-${AP_JDK_DL_VERSION}_${ap_os}-x64_bin.tar.gz" "${ap_jdk_dl_dir}"
-    curl -SOL "https://download.oracle.com/java/${AP_JDK_DL_VERSION}/latest/jdk-${AP_JDK_DL_VERSION}_${ap_os}-x64_bin.tar.gz"
-    tar -zxf jdk-${AP_JDK_DL_VERSION}_${ap_os}-x64_bin.tar.gz
+    rm -rf "jdk-${AP_JDK_SETUP_VERSION}_${ap_os}-x64_bin.tar.gz" "${ap_jdk_dl_dir}"
+    curl -SOL "https://download.oracle.com/java/${AP_JDK_SETUP_VERSION}/latest/jdk-${AP_JDK_SETUP_VERSION}_${ap_os}-x64_bin.tar.gz"
+    tar -zxf jdk-${AP_JDK_SETUP_VERSION}_${ap_os}-x64_bin.tar.gz
 
     rm -rf "${ap_vendors_java_dir}"
     mkdir -p "${AP_SOFT_DIR}/java"
     mv "${ap_jdk_dl_dir}" "${ap_vendors_java_dir}"
 
-    # Load jdk init script
     @initjava
+    if alias @createdirstructjava &>/dev/null; then
+        @createdirstructjava
+    fi
 }
 
 alias @rmjava="ap_func_rm_java"
 ap_func_rm_java() {
     @logshow "Remove [java]\n"
     rm -rf "${AP_SOFT_DIR}/java"
+
+    if alias @rmdirstructjava &>/dev/null; then
+        @rmdirstructjava
+    fi
+
+    if alias @rmglobalsymlinkjava &>/dev/null; then
+        @rmglobalsymlinkjava
+    fi
 }
