d96d6a80d8938e139713c9007c194aa525652e82
Albert Pham <pnphuong29@gmail.com>
2024-01-06 18:45:25 +0700

    Auto commit

--- ap_master_root.sh
+++ ap_master_root.sh
@@ -38,5 +38,13 @@ alias l3="@exa --tree --level 3"
 alias ld3="@exa --tree --level 3 -D"
 alias lg="@exa --all --git"
 
+# tree
+alias t='tree -ugpshaC -I .git -I node_modules'                               # list all hidden files & dirs & ignore dirs (.git, etc.)
+alias t1='tree -L 1 -ugpshaC --metafirst --dirsfirst -I .git -I node_modules' # list level 1 of all hidden files & dirs & ignore dirs (.git, etc.)
+alias t2='tree -L 2 -ugpshaC --metafirst --dirsfirst -I .git -I node_modules' # list level 2 of all hidden files & dirs & ignore dirs (.git, etc.)
+alias t3='tree -L 3 -ugpshaC --metafirst --dirsfirst -I .git -I node_modules' # list level 2 of all hidden files & dirs & ignore dirs (.git, etc.)
+alias t4='tree -L 4 -ugpshaC --metafirst --dirsfirst -I .git -I node_modules' # list level 2 of all hidden files & dirs & ignore dirs (.git, etc.)
+alias t5='tree -L 5 -ugpshaC --metafirst --dirsfirst -I .git -I node_modules' # list level 2 of all hidden files & dirs & ignore dirs (.git, etc.)
+
 # joshuto
 bind -x '"\C- ":"joshuto"'
