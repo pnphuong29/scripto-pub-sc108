ea685064f1f0bfc3669bc6e3a9dd24c2876a4d4c
Albert Pham <pnphuong29@gmail.com>
2024-01-06 18:50:48 +0700

    Auto commit

--- ap_master_root.sh
+++ ap_master_root.sh
@@ -33,7 +33,7 @@ fi
 # https://unix.stackexchange.com/questions/105958/terminal-prompt-not-wrapping-correctly
 export PS1_OLD="${PS1}"
 # export PS1="\[\e[95;49m\]\t \[\e[32;49m\]\w\[\e[m\]\$ "
-export PS1="\[\e[95;49m\]\t \[\e[93;49m\]\u@\h \[\e[32;49m\]\W\[\e[m\]\#\#\# "
+export PS1="\[\e[95;49m\]\t \[\e[93;49m\]\u@\h \[\e[32;49m\]\W\[\e[m\]### "
 
 # exa
 alias @exa="exa -a --long --header --icons --group --group-directories-first"
