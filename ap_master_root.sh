2b2fff2a3d7d7f5a334142b0520987a0b17a677d
Albert Pham <pnphuong29@gmail.com>
2024-01-06 18:48:55 +0700

    Auto commit

--- ap_master_root.sh
+++ ap_master_root.sh
@@ -28,6 +28,13 @@ if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
     [ ! -L /usr/local/bin/md5 ] && sudo ln -sf /usr/bin/md5sum /usr/local/bin/md5
 fi
 
+# Bash prompt
+# https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html
+# https://unix.stackexchange.com/questions/105958/terminal-prompt-not-wrapping-correctly
+export PS1_OLD="${PS1}"
+# export PS1="\[\e[95;49m\]\t \[\e[32;49m\]\w\[\e[m\]\$ "
+export PS1="\[\e[95;49m\]\t \[\e[93;49m\]\u@\h \[\e[32;49m\]\W\[\e[m\]\#\#\# "
+
 # exa
 alias @exa="exa -a --long --header --icons --group --group-directories-first"
 alias l="@exa --all"
