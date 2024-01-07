8d1ba1677a11939895f3f5c7e1648b4fada7b0f4
Albert Pham <pnphuong29@gmail.com>
2024-01-07 12:26:13 +0700

    Auto commit

--- libs/ap_env_dir_struct.sh
+++ libs/ap_env_dir_struct.sh
@@ -1,3 +1,15 @@
+# Logs
+export AP_LOGS_DIR="${HOME}/scripto-data/logs"
+
+## General logs
+export AP_LOGS_GENERAL_FILE="${AP_LOGS_DIR}/general/ap_general.log"
+
+## Error logs
+export AP_LOGS_ERROR_FILE="${AP_LOGS_DIR}/general/ap_error.log"
+
+## Debug logs
+export AP_LOGS_DEBUG_FILE="${AP_LOGS_DIR}/general/ap_debug.log"
+
 # Symlinks
 export AP_SYMLINKS_DIR="${HOME}/scripto-data/symlinks"
 
@@ -18,15 +30,3 @@ export AP_PRJ_DIR="${HOME}/scripto-data/projects"
 
 # Tmp
 export AP_TMP_DIR="${HOME}/scripto-data/tmp"
-
-# Logs
-export AP_LOGS_DIR="${HOME}/scripto-data/logs"
-
-## General logs
-export AP_LOGS_GENERAL_FILE="${AP_LOGS_DIR}/general/ap_general.log"
-
-## Error logs
-export AP_LOGS_ERROR_FILE="${AP_LOGS_DIR}/general/ap_error.log"
-
-## Debug logs
-export AP_LOGS_DEBUG_FILE="${AP_LOGS_DIR}/general/ap_debug.log"
