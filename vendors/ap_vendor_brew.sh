0e1dd18df5d46ec85adf8142019a18f8d84c98e8
Albert Pham <pnphuong29@gmail.com>
2024-02-16 16:30:46 +0700

    Update ap_vendor_brew.sh

--- vendors/ap_vendor_brew.sh
+++ vendors/ap_vendor_brew.sh
@@ -1,16 +1,16 @@
 alias @initbrew="ap_func_init_brew"
 ap_func_init_brew() {
     # https://docs.brew.sh/Shell-Completion
-    if type brew &>/dev/null; then
-        HOMEBREW_PREFIX="$(brew --prefix)"
-        if [ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]; then
-            source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-        else
-            for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-                [ -r "${COMPLETION}" ] && source "${COMPLETION}"
-            done
-        fi
-    fi
+    # if type brew &>/dev/null; then
+    #     HOMEBREW_PREFIX="$(brew --prefix)"
+    #     if [ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]; then
+    #         source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    #     else
+    #         for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+    #             [ -r "${COMPLETION}" ] && source "${COMPLETION}"
+    #         done
+    #     fi
+    # fi
 
     if alias @initbrewshare &>/dev/null; then
         @initbrewshare
