e7a441e1fb14d966da9806216ce6321fd3614dee
Albert Pham <pnphuong29@gmail.com>
2024-04-08 19:37:29 +0700

    Auto commit

--- vendors/ap_vendor_1password.sh
+++ vendors/ap_vendor_1password.sh
@@ -58,16 +58,16 @@ ap_func_setup_1password() {
     elif [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
         sudo -s \
             curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-            gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+            sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-            tee /etc/apt/sources.list.d/1password.list
-        mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+            sudo tee /etc/apt/sources.list.d/1password.list
+        sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
         curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
-            tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-        mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+            sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+        sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
         curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-            gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-        apt update && apt install 1password-cli
+            sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+        sudo apt update && sudo apt install 1password-cli
     fi
 
     apinit1password
