fca78e0cee4bfab96e5a8997ffe252430ab3a0a7
Albert Pham <pnphuong29@gmail.com>
2024-06-02 14:53:15 +0700

    Auto commit

--- vendors/ap_vendor_postman.sh
+++ vendors/ap_vendor_postman.sh
@@ -15,22 +15,24 @@ ap_func_init_postman() {
 
 alias apcreatedirstructpostman="ap_func_create_dirstruct_postman"
 ap_func_create_dirstruct_postman() {
-    aplogshow "Generate [postman] bash autocomplete\n"
-    postman >"${AP_COMPLETIONS_DIR}/ap_completion_postman.bash"
+    if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
+        # Create a symbolic link to the Postman executable
+        aplogshow "Creating symbolic link from [${AP_SOFT_DIR}/bin/postman] to [${AP_SOFT_DIR}/postman/Postman]\n"
+        ln -sf "${AP_SOFT_DIR}/Postman" "${AP_SOFT_DIR}/bin/postman"
 
-    if [ -f "${HOME}/scripto-share/vendors/postman/bindings.json" ]; then
-        aplogshow "Create symlink from [${HOME}/.config/postman/bindings.json] to [${HOME}/scripto-share/vendors/postman/bindings.json]\n"
-        ln -sf "${HOME}/scripto-share/vendors/postman/bindings.json" "${HOME}/.config/postman/bindings.json"
+        # Create a desktop entry for Postman
+        aplogshow "Creating desktop entry at [${HOME}/.local/share/applications/postman.desktop]\n"
+        cat <<-EOL | tee "${HOME}/.local/share/applications/postman.desktop" >/dev/null
+[Desktop Entry]
+Name=Postman
+Comment=Make and view API requests
+Exec=${AP_SOFT_DIR}/postman/Postman
+Icon=${AP_SOFT_DIR}/postman/app/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;Utility;
+EOL
     fi
-
-    aplogshow "Create symlink from [${AP_SOFT_DIR}/bin/postman] to [${AP_SOFT_DIR}/postman/bin/postman]\n"
-    ln -sf "${AP_SOFT_DIR}/postman/bin/postman" "${AP_SOFT_DIR}/bin/postman"
-
-    aplogshow "Create symlink from [${AP_COMPLETIONS_DIR}/ap_completion_postman.bash] to [${AP_SOFT_DIR}/postman/complete/postman.bash]\n"
-    ln -sf "${AP_SOFT_DIR}/postman/autocomplete/postman.bash" "${AP_COMPLETIONS_DIR}/ap_completion_postman.bash"
-
-    aplogshow "Create symlink from [${AP_MAN_DIR}/man1/postman.1] to [${AP_SOFT_DIR}/postman/postman.1]\n"
-    ln -sf "${AP_SOFT_DIR}/postman/postman.1" "${AP_MAN_DIR}/man1/postman.1"
 
     if alias apcreatedirstructpostmanshare &>/dev/null; then
         apcreatedirstructpostmanshare
@@ -47,14 +49,8 @@ ap_func_create_dirstruct_postman() {
 
 alias aprmdirstructpostman="ap_func_rm_dirstruct_postman"
 ap_func_rm_dirstruct_postman() {
-    aplogshow "Remove [${AP_SOFT_DIR}/bin/postman]\n"
     rm -f "${AP_SOFT_DIR}/bin/postman"
-
-    aplogshow "Remove [${AP_COMPLETIONS_DIR}/ap_completion_postman.bash]\n"
-    rm -f "${AP_COMPLETIONS_DIR}/ap_completion_postman.bash"
-
-    aplogshow "Remove [${AP_MAN_DIR}/man1/postman.1]\n"
-    rm -f "${AP_MAN_DIR}/man1/postman.1"
+    rm -f "${HOME}/.local/share/applications/postman.desktop"
 
     if alias aprmdirstructpostmanshare &>/dev/null; then
         aprmdirstructpostmanshare
@@ -69,85 +65,63 @@ ap_func_rm_dirstruct_postman() {
     fi
 }
 
-alias apcreateglobalsymlinkpostman="ap_func_create_global_symlink_postman"
-ap_func_create_global_symlink_postman() {
-    if [ -f "${AP_SOFT_DIR}/bin/postman" ]; then
-        aplogshow "Create symlink from [/usr/local/bin/postman] to [${AP_SOFT_DIR}/bin/postman]\n"
-        sudo ln -sf "${AP_SOFT_DIR}/bin/postman" "/usr/local/bin/postman"
-    fi
-}
-
-alias aprmglobalsymlinkpostman="ap_func_rm_global_symlink_postman"
-ap_func_rm_global_symlink_postman() {
-    if [ -f "/usr/local/bin/postman" ]; then
-        aplogshow "Remove [/usr/local/bin/postman]\n"
-        sudo rm -f "/usr/local/bin/postman"
-    fi
-}
-
 alias apsetuppostman="ap_func_setup_postman"
 ap_func_setup_postman() {
-    local ap_postman_setup_version=''
-    if [ -n "$1" ]; then
-        ap_postman_setup_version="$1"
-    fi
-
-    aplogshow "Install [postman] v${ap_postman_setup_version}\n"
-
     aplogshow "Install [postman]\n"
 
-    # Remove old app dir if any
-    rm -rf "${AP_SOFT_DIR}/postman"
-    rm -rf "${AP_TMP_DIR}/postman"
-
-    # Install postman
-    mkdir -p "${AP_TMP_DIR}/postman"
-    cd "${AP_TMP_DIR}/postman"
-
     if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_MACOS}" ]; then
-        if [[ "$(uname -m)" == 'arm64' ]]; then
-            curl -SL \
-                "$(curl --silent "https://api.github.com/repos/owner/postman/releases" | jq -r '.[0].assets[].browser_download_url' | grep "macos" | grep arm | grep -v sha256)" >postman.tar.gz
-        elif [[ "$(uname -m)" == 'x86_64' ]]; then
-            curl -SL \
-                "$(curl --silent "https://api.github.com/repos/owner/postman/releases" | jq -r '.[0].assets[].browser_download_url' | grep "macos" | grep x86_64 | grep -v sha256)" >postman.tar.gz
+        # Define the URL for the latest Postman tarball
+        POSTMAN_URL="https://dl.pstmn.io/download/latest/osx"
+
+        # Define the download target file
+        TARGET_FILE="${AP_TMP_DIR}/postman.zip"
+
+        # Define the target directory
+        TARGET_DIR="/Applications/Postman.app"
+
+        # Download the Postman zip file
+        aplogshow "Downloading Postman...\n"
+        curl -o "${TARGET_FILE}" "${POSTMAN_URL}"
+
+        # Remove any existing Postman application
+        if [ -d "${TARGET_DIR}" ]; then
+            aplogshow "Removing existing Postman application [/Applications/Postman.app]"
+            rm -rf "${TARGET_DIR}"
         fi
 
-        unzip postman.zip
-        mv postman* postman
-        rm -rf "/Applications/postman"
-        mv "postman/postman.app" /Applications/
-        cd "/Applications"
+        # Unzip the downloaded file to the Applications directory
+        aplogshow "Extracting Postman...\n"
+        unzip "${TARGET_FILE}" -d /Applications
 
-        hdiutil attach -nobrowse postman.dmg
-        cd "/Volumes/postman"
-        cp -R "postman.app" /Applications/
-        cd ~
-        hdiutil detach "/Volumes/postman"
+        # Clean up the zip file
+        aplogshow "Cleaning up...\n"
+        rm "${TARGET_FILE}"
     elif [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
-        curl -SL \
-            "$(curl --silent "https://api.github.com/repos/owner/postman/releases" | jq -r '.[0].assets[].browser_download_url' | grep "linux" | grep x86_64 | grep -v sha256 | grep musl)" >postman.tar.gz
-        sudo dpkg -i postman.deb
+        # Define the URL for the latest Postman tarball
+        POSTMAN_URL="https://dl.pstmn.io/download/latest/linux64"
+
+        # Download the Postman tarball
+        aplogshow "Downloading Postman...\n"
+        wget -O "${AP_TMP_DIR}/postman.tar.gz" "${POSTMAN_URL}"
+
+        # Remove any existing Postman directory
+        if [ -d "${AP_SOFT_DIR}/postman" ]; then
+            aplogshow "Removing existing Postman directory [${AP_SOFT_DIR}/postman]\n"
+            rm -rf "${AP_SOFT_DIR}/postman"
+        fi
+
+        # Create the target directory
+        aplogshow "Creating target directory [${AP_SOFT_DIR}/postman]\n"
+        mkdir -p "${AP_SOFT_DIR}/postman"
+
+        # Extract the tarball to the target directory
+        aplogshow "Extracting Postman...\n"
+        tar -xzvf "${AP_TMP_DIR}/postman.tar.gz" -C "${AP_SOFT_DIR}"
+
+        # Clean up the tarball
+        aplogshow "Cleaning up...\n"
+        rm "${AP_TMP_DIR}/postman.tar.gz"
     fi
-
-    local ap_os="macos"
-    local ap_os="darwin"
-    if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
-        ap_os="linux"
-    fi
-
-    curl -SL \
-        "$(curl --silent https://api.github.com/repos/owner/postman/releases | jq -r '.[0].assets[].browser_download_url' | grep "${ap_os}" | grep x86_64 | grep -v sha256)" >postman.tar.gz
-
-    tar -zxf postman.tar.gz
-    rm -f postman.tar.gz
-    mv postman* postman
-    mv postman "${AP_SOFT_DIR}/"
-    cd "${AP_SOFT_DIR}/postman"
-    rm -rf "${AP_TMP_DIR}/postman"
-
-    pip install postman
-    npm install -g postman
 
     apinitpostman
     if alias apcreatedirstructpostman &>/dev/null; then
@@ -157,28 +131,12 @@ ap_func_setup_postman() {
 
 alias aprmpostman="ap_func_rm_postman"
 ap_func_rm_postman() {
-    local ap_postman_remove_version=''
-    if [ -n "$1" ]; then
-        ap_postman_remove_version="$1"
-    fi
-
-    aplogshow "Remove [postman] v${ap_postman_remove_version}\n"
-
     aplogshow "Remove [postman]\n"
-    pip uninstall postman
-    npm uninstall -g postman
-
-    rm -rf "${GOPATH}/pkg/mod/github.com/postman/postman@"*
-    rm -f "${GOPATH}/bin/postman"
-
-    rm -rf "${AP_SOFT_DIR}/postman"
 
     if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_MACOS}" ]; then
-        brew remove --cask postman
+        rm -rf "/Applications/Postman.app"
     elif [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
-        sudo apt purge -y postman
-        sudo snap remove --purge postman
-        sudo dpkg --purge postman
+        rm -rf "${AP_SOFT_DIR}/postman"
     fi
 
     if alias aprmdirstructpostman &>/dev/null; then
