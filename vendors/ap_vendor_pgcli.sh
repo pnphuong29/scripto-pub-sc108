8a30c926317e5369570e7896bc365ec8cfc2d0e6
Albert Pham <pnphuong29@gmail.com>
2024-10-13 22:32:18 +0700

    Auto commit

--- vendors/ap_vendor_pgcli.sh
+++ vendors/ap_vendor_pgcli.sh
@@ -58,29 +58,3 @@ ap_func_rm_pgcli() {
         aprmglobalsymlinkpgcli
     fi
 }
-
-alias pgclif='ap_func_pgcli'
-# @$func $$ ap_func_pgcli {
-# ap_func_pgcli *<host> *<port> *<user> *<pass> <db>
-# Description
-# 	Quickly connect to PG DB using short syntax
-# Return status
-#	AP_CODE_SUCCESS
-# }
-ap_func_pgcli() {
-    if [ "$#" -lt 4 ]; then
-        rtn_error_not_enough_arguments
-    fi
-
-    local ap_host="$1"
-    local ap_port="$2"
-    local ap_user="$3"
-    local ap_pass="$4"
-    local ap_db="${5:-}"
-
-    if [ -n "${ap_db}" ]; then
-        PGPASSWORD=${ap_pass} pgcli -h "${ap_host}" -p "${ap_port}" -u "${ap_user}" -d "${ap_db}"
-    else
-        PGPASSWORD=${ap_pass} pgcli -h "${ap_host}" -p "${ap_port}" -u "${ap_user}"
-    fi
-}
