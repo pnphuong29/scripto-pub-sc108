f04ca2c0cd1ead6e1e198cfedafd4d08ba65c7f8
Albert Pham <pnphuong29@gmail.com>
2025-01-25 06:46:10 +0700

    Auto commit

--- vendors/ap_vendor_3.0_fnm.sh
+++ vendors/ap_vendor_3.0_fnm.sh
@@ -6,12 +6,12 @@ ap_func_init_fnm() {
     alias zfnmnodeversions="cd \${FNM_DIR}/node-versions"
 
     if [ -f "${HOME}/.cargo/bin/fnm" ]; then
-        eval "$(
-            "${HOME}/.cargo/bin/fnm" env --use-on-cd --shell bash
-        )"
         # eval "$(
-        #     "${HOME}/.cargo/bin/fnm" env --use-on-cd --version-file-strategy=recursive --shell bash
+        #     "${HOME}/.cargo/bin/fnm" env --use-on-cd --version-file-strategy=local --shell bash
         # )"
+        eval "$(
+            "${HOME}/.cargo/bin/fnm" env --use-on-cd --version-file-strategy=recursive --shell bash
+        )"
     fi
 
     if alias apinitfnmshare &>/dev/null; then
