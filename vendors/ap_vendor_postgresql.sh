65ea3a4ba1ad59dcdabc8f678730bd83ef5e8f44
Albert Pham <pnphuong29@gmail.com>
2025-03-12 22:57:23 +0700

    Auto commit

--- vendors/ap_vendor_postgresql.sh
+++ vendors/ap_vendor_postgresql.sh
@@ -2,6 +2,8 @@
 # https://www.postgresql.org/download/macosx/
 alias apinitpostgresql="ap_func_init_postgresql"
 ap_func_init_postgresql() {
+    export AP_PG_SETUP_VERSION='16'
+
     if alias apinitpostgresqlshare &>/dev/null; then
         apinitpostgresqlshare
     fi
@@ -35,12 +37,17 @@ ap_func_rm_dirstruct_postgresql() {
 
 alias apsetuppostgresql="ap_func_setup_postgresql"
 ap_func_setup_postgresql() {
-    aplogshow "Install [PostgreSQL v16]\n"
+    local ap_pg_setup_version="${AP_PG_SETUP_VERSION}"
+    if [ -n "$1" ]; then
+        ap_pg_setup_version="$1"
+    fi
+
+    apshowmsginfo "Install [PostgreSQL] version [${ap_pg_setup_version}]\n"
 
     if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_MACOS}" ]; then
-        brew install postgresql@16
+        brew install "postgresql@${ap_pg_setup_version}"
     elif [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
-        sudo apt install -y postgresql-16
+        sudo apt install -y "postgresql-${ap_pg_setup_version}"
         # sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
         # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
         # sudo apt update
@@ -59,10 +66,15 @@ ap_func_setup_postgresql() {
 
 alias aprmpostgresql="ap_func_rm_postgresql"
 ap_func_rm_postgresql() {
-    aplogshow "Remove [PostgreSQL]\n"
+    local ap_pg_remove_version="${AP_PG_SETUP_VERSION}"
+    if [ -n "$1" ]; then
+        ap_pg_remove_version="$1"
+    fi
+
+    apshowmsginfo "Remove [PostgreSQL] v${ap_pg_remove_version}\n"
 
     if [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_MACOS}" ]; then
-        brew uninstall postgresql
+        brew uninstall "postgresql@${ap_pg_remove_version}"
     elif [ "${AP_OS_TYPE}" == "${AP_OS_TYPE_UBUNTU}" ]; then
         local ap_pgsql_major_version
         ap_pgsql_major_version="$(psql --version | cut -d ' ' -f 3 | cut -d . -f 1)"
